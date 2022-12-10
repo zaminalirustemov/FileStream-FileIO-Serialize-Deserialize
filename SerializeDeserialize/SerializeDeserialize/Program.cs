617f10dce567262c2f4bd8baa86337c70e108522
Zaminali <2g17rck@code.eddu.az>
2022-12-10 22:14:55 +0400

    HW

---
+++ SerializeDeserialize/SerializeDeserialize/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.IO;
+using System.Net.Http.Json;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace SerializeDeserialize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////----------------||BinaryFormatter||------------------------------------------------------------
+            User user = new User();
+            user.Id = 1;
+            user.Name = "Zaminali";
+            user.Surname = "Rustemov";
+
+            SerializableToBinary(user);
+
+            //User user1 = DeserializableToBinary();
+            //Console.WriteLine(user1);
+
+
+            //////////----------------||XMLFormatter||----------------------------------------------------------------
+            User user2 = new User();
+            user2.Id = 2;
+            user2.Name = "Namiq";
+            user2.Surname = "Mikayilov";
+
+            SerializableToXML(user2);
+
+            //User user3 = DeserializableToXMl();
+            //Console.WriteLine(user3);
+
+
+            //////////----------------||JSONFormatter||----------------------------------------------------------------
+            User user4 = new();
+            user4.Id = 3;
+            user4.Name = "Ali";
+            user4.Surname = "Ibrahimov";
+
+            SerializableToJson(user4);
+
+            //User user6 = DeserializableToJson();
+            //Console.WriteLine(user6);
+
+
+        }
+
+
+        //////////----------------||BinaryFormatter||------------------------------------------------------------
+        public static void SerializableToBinary(User user)
+        {
+            string path = "C:\\Users\\HP\\Desktop\\User.dat";
+            Stream stream = new FileStream(path, FileMode.Create);
+
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(stream, user);
+        }
+        public static User DeserializableToBinary()
+        {
+            string path = "C:\\Users\\HP\\Desktop\\User.dat";
+            Stream stream = new FileStream(path, FileMode.Open);
+
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            return (User)binaryFormatter.Deserialize(stream);
+        }
+
+
+        //////////----------------||XMLFormatter||----------------------------------------------------------------
+        public static void SerializableToXML(User user)
+        {
+            string path = "C:\\Users\\HP\\Desktop\\User2.xml";
+            Stream stream = new FileStream(path, FileMode.Create);
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(User));
+            xmlSerializer.Serialize(stream, user);
+        }
+
+        public static User DeserializableToXMl()
+        {
+            string path = "C:\\Users\\HP\\Desktop\\User2.xml";
+            Stream stream = new FileStream(path, FileMode.Open);
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(User));
+            return (User)xmlSerializer.Deserialize(stream);
+        }
+
+
+        //////////----------------||JSONFormatter||----------------------------------------------------------------
+        public static void SerializableToJson(User user)
+        {
+            string path = "C:\\Users\\HP\\Desktop\\User.json";
+
+            string json = JsonSerializer.Serialize(user);
+            File.WriteAllText(path, json);
+        }
+        public static User DeserializableToJson()
+        {
+            string path = "C:\\Users\\HP\\Desktop\\User.json";
+            Stream stream = new FileStream(path, FileMode.Open);
+
+            User user=JsonSerializer.Deserialize<User>(stream);
+            return user;
+        }
+    }
+}
---
+++ SerializeDeserialize/SerializeDeserialize/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeDeserialize
+{
+    [Serializable]
+    public class User : ISerializable
+    {
+        public User(SerializationInfo info, StreamingContext context)
+        {
+            Id = (int)info.GetValue("Id", typeof(int));
+            Name = (string)info.GetValue("Name",typeof(string));
+            Surname = (string)info.GetValue("Surname",typeof(string));
+        }
+
+        public User()
+        {
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Id",Id,typeof(int));
+            info.AddValue("Name", Name, typeof(string));
+            info.AddValue("Surname", Surname, typeof(string));
+        }
+        public override string ToString()
+        {
+            return $" ID: {Id}\n Name: {Name}\n Surname: {Surname}";
+        }
+    }
+}
